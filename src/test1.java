1347aec7d0bd2bb6d353cb2c09d5af91bdfc0ddd
liuquanwei <liuquanwei@qq.com>
2022-05-01 14:37:48 +0800

    second commit

--- src/test1.java
+++ src/test1.java
@@ -3,5 +3,6 @@ public class test1 {
         System.out.println(test1.class.getClassLoader());
         System.out.println(test1.class.getClassLoader().getParent());
         System.out.println(test1.class.getClassLoader().getParent().getParent());
+        System.out.println("你好");
     }
 }
